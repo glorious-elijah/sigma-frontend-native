6e4cafb82cd670cb3e95722bac3b0b54a4bd6e96
Glorious <gloriouselijah125@gmail.com>
2025-04-16 06:20:00 +0000

    :hammer: Add trunk check on commit and push
    Run trunk check --all --fix to identify and fix existing linting issues
    in boilerplate
    
     Changes to be committed:
    	modified:   .detoxrc.js
    	modified:   .expo/README.md
    	modified:   .expo/types/router.d.ts
    	modified:   .gitignore
    	new file:   .trunk/configs/.yamllint.yaml
    	modified:   .trunk/trunk.yaml
    	modified:   app/(tabs)/_layout.tsx
    	modified:   app/(tabs)/explore.tsx
    	modified:   app/+html.tsx
    	modified:   app/+not-found.tsx
    	modified:   assets/images/favicon.png
    	modified:   assets/images/partial-react-logo.png
    	modified:   assets/images/react-logo.png
    	modified:   assets/images/react-logo@2x.png
    	modified:   assets/images/react-logo@3x.png
    	modified:   assets/images/splash.png
    	modified:   components/Collapsible.tsx
    	modified:   components/ExternalLink.tsx
    	modified:   components/HelloWave.tsx
    	modified:   components/ParallaxScrollView.tsx
    	modified:   components/ThemedText.tsx
    	modified:   components/ThemedView.tsx
    	modified:   components/__tests__/ThemedText-test.tsx
    	modified:   components/navigation/TabBarIcon.tsx
    	modified:   constants/Colors.ts
    	modified:   e2e/jest.config.js
    	modified:   e2e/starter.test.js
    	modified:   hooks/useColorScheme.ts
    	modified:   hooks/useColorScheme.web.ts
    	modified:   hooks/useThemeColor.ts
    	modified:   nativewind-env.d.ts
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   scripts/reset-project.js
    	modified:   tsconfig.json

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -1,12 +1,19 @@
 /* eslint-disable */
-import * as Router from 'expo-router';
+import * as Router from "expo-router";
 
-export * from 'expo-router';
+export * from "expo-router";
 
-declare module 'expo-router' {
+declare module "expo-router" {
   export namespace ExpoRouter {
-    export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/explore` | `/_sitemap` | `/explore`;
+    export interface __routes<T extends string = string>
+      extends Record<string, unknown> {
+      StaticRoutes:
+        | `/`
+        | `/(tabs)`
+        | `/(tabs)/`
+        | `/(tabs)/explore`
+        | `/_sitemap`
+        | `/explore`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
